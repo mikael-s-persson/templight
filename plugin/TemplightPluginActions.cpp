23f8aeee243b4d1bfdbb41d59529830547cb72cc
S. Mikael Persson <mikael.s.persson@gmail.com>
2014-12-23 14:43:21 -0500

    Started this branch to develop templight as a plugin such that it might be easier eventually to inject templight functionality without having to copy-paste all the clang frontend / driver.

---
+++ plugin/TemplightPluginActions.cpp
@@ -0,0 +1,20 @@
+//===- TemplightAction.cpp ------ Clang Templight Frontend Action -*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "TemplightPluginActions.h"
+#include <TemplightAction.h>
+#include <TemplightTracer.h>
+#include <TemplightDebugger.h>
+
+namespace clang {
+
+
+
+}
+
---
+++ plugin/TemplightPluginActions.h
@@ -0,0 +1,26 @@
+//===- TemplightAction.h ------ Clang Templight Frontend Action -*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TEMPLIGHT_TEMPLIGHT_PLUGIN_ACTIONS_H
+#define LLVM_CLANG_TEMPLIGHT_TEMPLIGHT_PLUGIN_ACTIONS_H
+
+#include <TemplightAction.h>
+
+namespace clang {
+
+
+class TemplightSyntaxOnlyAction : public TemplightWrapPluginAction {
+  
+  
+};
+
+
+}
+
+#endif
