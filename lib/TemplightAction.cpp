d4b9b1bb22fe49bb275f7a74d17a4ce45e42509b
S. Mikael Persson <mikael.s.persson@gmail.com>
2014-12-23 14:39:26 -0500

    Fixed an issue with the non-existence of the source-manager under some options (-fsyntax-only for example).

--- lib/TemplightAction.cpp
+++ lib/TemplightAction.cpp
@@ -55,9 +55,12 @@ std::string TemplightAction::CreateOutputFilename(
   // Should never get executed. 
   if ( CI && result.empty() ) {
     // then, derive output name from the input name:
-    FileID fileID = CI->getSourceManager().getMainFileID();
-    result =
-      CI->getSourceManager().getFileEntryForID(fileID)->getName();
+    if ( CI->hasSourceManager() ) {
+      FileID fileID = CI->getSourceManager().getMainFileID();
+      result = CI->getSourceManager().getFileEntryForID(fileID)->getName();
+    } else { // or, last resort:
+      result = "a";
+    }
   }
   
   std::string postfix;
