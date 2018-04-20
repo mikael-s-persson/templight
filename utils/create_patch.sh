36e175e0fcc695bb58a0be3093715b2e92a9977c
Kristóf Umann <dkszelethus@gmail.com>
2018-04-20 10:01:39 +0200

    Generatig patches creates a version.txt file

--- utils/create_patch.sh
+++ utils/create_patch.sh
@@ -9,7 +9,25 @@ export LC_MESSAGES=C
 export LC_COLLATE=C
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd "${SCRIPTPATH}/../../.."
+cd "${SCRIPTPATH}/../../../../../"
 
-git diff > tools/templight/templight_clang_patch.diff
-git diff -U999999 > tools/templight/templight_clang_patch_with_context.diff
+VERSIONFILE="${SCRIPTPATH}/../templight_clang_version.txt"
+
+LLVMGIT="$(git log -1 | grep "commit .*" | awk -v N=2 '{print $N}')"
+LLVMSVN="$(git log -1 | grep "git-svn-id: .*" | awk -v N=2 '{print $N}')"
+cd tools/clang/
+CLANGGIT="$(git log -1 | grep "commit .*" | awk -v N=2 '{print $N}')"
+CLANGSVN="$(git log -1 | grep "git-svn-id: .*" | awk -v N=2 '{print $N}')"
+
+echo "'templight_clang_patch*.diff' was created using the following LLVM/Clang versions:\n" > $VERSIONFILE
+
+echo "LLVM git: $LLVMGIT" >> $VERSIONFILE
+echo "LLVM svn: $LLVMSVN" >> $VERSIONFILE
+
+echo "Clang git: $CLANGGIT" >> $VERSIONFILE
+echo "Clang svn: $CLANGSVN" >> $VERSIONFILE
+
+git diff > $SCRIPTPATH/../templight_clang_patch.diff
+git diff -U999999 > $SCRIPTPATH/../templight_clang_patch_with_context.diff
+
+
